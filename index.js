f35ebd836d633dbabe8e788e8e789b9ffb6ceaf1
ljusen <130145791+ljusen@users.noreply.github.com>
2023-04-07 09:28:05 -0300

    1

--- index.js
+++ index.js
@@ -2,21 +2,21 @@
 function selectPalette(palette) {
     if (palette === "dark"){
 
-        document.getElementById('colorway').href='../CSS/dark.css';//change css file used for palette-related styling
+        document.getElementById('colorway').href='/dark.css';//change css file used for palette-related styling
         document.getElementById('twitter').src='/images/smLinks/twitDark.png';//replace twitter link according to theme
         document.getElementById('instagram').src='/images/smLinks/instaDark.png';//replace insta link according to theme
         document.getElementById('email').src='/images/smLinks/mailDark.png';//replace mail link according to theme
     }
     //same function as above, accounts for different input
     else if (palette == 'light'){
-        document.getElementById('colorway').href='../CSS/light.css';
+        document.getElementById('colorway').href='/light.css';
         document.getElementById('twitter').src='/images/smLinks/twitLight.png';
         document.getElementById('instagram').src='/images/smLinks/instaLight.png';
         document.getElementById('email').src='/images/smLinks/mailLight.png';
     }
     //same function as above, accounts for different input
     else if (palette == 'coffee'){
-        document.getElementById('colorway').href='../CSS/coffee.css';
+        document.getElementById('colorway').href='/coffee.css';
         document.getElementById('twitter').src='/images/smLinks/twitCoffee.png';
         document.getElementById('instagram').src='/images/smLinks/instaCoffee.png';
         document.getElementById('email').src='/images/smLinks/mailCoffee.png';
@@ -25,7 +25,7 @@ function selectPalette(palette) {
 
 
 //fetch JSON file, get data
-fetch('../user.json').then(function (response) {
+fetch('/user.json').then(function (response) {
     return response.json();
 }).then(function (obj) { //turn data into a javascript object
     selectPalette(obj.Colorway); //select default palette based on the data in user.json
